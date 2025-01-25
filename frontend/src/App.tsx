20dda323ee51baf93cc12f312c6dae64a46b4aa8
Pedro Kohler <pedrokohlerbh@gmail.com>
2025-01-24 21:43:14 -0300

    fix: file names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { Stack } from "react-bootstrap";
 import { CountdownSection } from "./Components/Sections/Countdown/CountdownSection";
 import { HeroSection } from "./Components/Sections/HeroSection";
 import { AboutSection } from "./Components/Sections/AboutSection";
-import { LocationsSection } from "./Components/Sections/LocationsSections";
+import { LocationsSection } from "./Components/Sections/LocationsSection";
 import { RSVPSection } from "./Components/Sections/RSVP/RSVPSection";
-import { GiftsSection } from "./Components/Sections/GiftsSections";
+import { GiftsSection } from "./Components/Sections/GiftsSection";
 
 function App() {
   return (
---
+++ frontend/src/Components/Sections/MessageSection.tsx
@@ -0,0 +1,39 @@
+import Stack from "react-bootstrap/Stack";
+import { useMediaQuery } from "usehooks-ts";
+
+import FlowerSeparator from "../../assets/separador2.svg";
+import { PageSeparator } from "../PageSeparator";
+
+export const GiftsSection = () => {
+  const isAbove750w = useMediaQuery("(min-width: 750px)");
+  const isAbove1000w = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Stack
+      gap={5}
+      style={{
+        height: "fit-content",
+        width: "100%",
+        padding: isAbove1000w
+          ? "64px 256px"
+          : isAbove750w
+          ? "32px 128px"
+          : "32px 64px",
+      }}
+    >
+      <PageSeparator icon={FlowerSeparator} />
+      <h2>LISTA DE PRESENTES</h2>
+      <div
+        style={{
+          color: "black",
+          maxWidth: "80%",
+          alignSelf: "center",
+        }}
+      >
+        Donec dignissim et libero sed congue. Cras sit amet vestibulum diam.
+        Etiam ut varius turpis. Proin luctus efficitur mi, nec sollicitudin
+        tellus tincidunt sed.
+      </div>
+    </Stack>
+  );
+};
