3f5c6828d28bb6e0f72ef656ce76d55d8d51fca1
Pedro Kohler <pedrokohlerbh@gmail.com>
2025-02-03 13:33:34 -0300

    chore: change children label

--- frontend/src/Components/Sections/RSVP/RSVPForm.tsx
+++ frontend/src/Components/Sections/RSVP/RSVPForm.tsx
@@ -37,7 +37,7 @@ const ChildrenInput = ({
         color: "black",
       }}
     >
-      <span>Crianças:</span>
+      <span>Crianças acima de 12 anos:</span>
       <IconButton
         icon={DashCircle}
         onClick={() => {
